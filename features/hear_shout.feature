cb84b83467f5b660950bf0b178e4082ccd3a0f6e
Eugene Chung <ycechung@mysenec.aca>
2023-02-15 15:11:37 -0500

    new v1

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -7,21 +7,24 @@ Feature: Hear shout
 
   - only shout to people within a certain distanceUSH:
     
-  Background:
-      Given a person named Lucy
-      And a person named Sean
-  
   Rule: Shouts can be heard by other users
 
     Scenario: Listener hears a message
-      
+      Given a person named Lucy is located at 0
+      And a person named Sean is located at 50
       When Sean shouts "free bagels at Sean's"
       Then Lucy should hear Sean's message
 
-    Scenario: Listener hears a different message
-      When Sean shouts "Free Coffee!"
-      Then Lucy should hear Sean's message
+  Rule: Shouts should only be heard if listened within range
 
     Scenario: Listener is within range
+      Given a person named Sean is located at 0
+      And a person named Lucy is located at 50
+      When Sean shouts
+      Then Lucy should hear Sean's message
 
     Scenario: Listener is out of range
+      Given a person named Sean is located at 0
+      And a person named Larry is located at 150
+      When Sean shouts
+      Then Larry should not hear Sean's message
--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -3,29 +3,48 @@ const { assertThat, is } = require('hamjest')
 
 const { Person, Network } = require("../../src/shouty");
 
+const default_range = 100;
+
 Before(function () {
-  this.network = new Network();
+  this.network = new Network(default_range);
   this.people = {};
 });
+
+Given("the range is {int}", function (range) {
+  this.network = new Network(range);
+});
+
 Given("a person named {word}", function (name) {
-  this.people[name] = new Person(this.network);
+  this.people[name] = new Person(this.network, 0);
 });
 
-/*
-Given("Lucy is {int} metres from Sean", function (distance) {
-  this.network = new Network()
-  this.lucy = new Person(this.network)
-  this.sean = new Person(this.network)
-
-  this.lucy.moveTo(distance)
+Given("a person named {word} is located at {int}", function (name, location) {
+  this.people[name] = new Person(this.network, location);
 });
-*/
+
+When("Sean shouts", function () {
+  this.people["Sean"].shout("Hello, world");
+  this.messageFromSean = "Hello, world";
+});
+
 When("Sean shout(s) {string}", function (message) {
   this.people["Sean"].shout(message);
   this.messageFromSean = message;
 });
 
+Then("{person} should not hear Sean's message", function (person) {
+  assertThat(this.people[person].messagesHeard(), is(this.messageFromSean));
+});
+
+Then("Lucy should hear a shout", function () {
+  assertThat(this.people["Lucy"].messagesHeard().length, is(1));
+});
+
 Then("Lucy should hear Sean's message", function () {
-  // Write code here that turns the phrase above into concrete actions
   assertThat(this.people["Lucy"].messagesHeard(), is([this.messageFromSean]));
+});
+
+Then("Larry should not hear Sean's message", function () {
+  // Write code here that turns the phrase above into concrete actions
+  assertThat(this.people["Larry"].messagesHeard(), is([this.messageFromSean]));
 });
